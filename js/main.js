0c389775574cb89da927a736c85f5d8279d82630
Mridul <thapliyamridul19@gmailcom>
2020-10-15 15:40:29 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -191,7 +191,7 @@
     autoplay: true,
     dots: true,
     loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 }
     }
   });
 
